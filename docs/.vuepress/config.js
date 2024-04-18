22e649fc1157c233fdf03d9c7429828c13c0e981
wanggq <wangguoqianglkw@gmail.com>
2024-04-18 19:37:18 +0800

    modify config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,36 @@
 const routeLayout = require('./routerLayout-test');
 
-
 const guideRoute = routeLayout("../guide")
 
-module.exports = {
-    title: 'Hello VuePress',
-	base: '/note/',
-    description: 'Just playing around',
+    module.exports = {
+    title: "Heihei's Blog",
+    base: '/note/',
+    description: "LIVE FOR LIVE",
 
     themeConfig: {
+        editLink: {
+            pattern: "https://github.com/vuejs/vitepress/edit/main/docs/:path",
+        },
+
+        nav: [{
+                text: "Home",
+                link: "/",
+            }, {
+                text: "Guide",
+                link: "/guide/",
+            }, {
+                text: "External",
+                link: "https://baidu.com",
+            },
+        ],
         sidebar: [
-            '/',
-            {
+            '/', {
                 "title": "Guide",
                 "children": guideRoute
-            },
-            {
+            }, {
                 "title": "面试",
                 "children": routeLayout("../面试")
-            },
-            {
+            }, {
                 "title": "Python",
                 "children": routeLayout("../Python")
             }
